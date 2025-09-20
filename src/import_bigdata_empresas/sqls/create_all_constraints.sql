71b9e362078c379e14387a041f160eef7ed0e79c
Adriano Moura <adrianodemoura@gmail.com>
2025-09-20 11:56:23 -0300

    correção ao checar e-mail e constraint

--- src/import_bigdata_empresas/sqls/create_all_constraints.sql
+++ src/import_bigdata_empresas/sqls/create_all_constraints.sql
@@ -2,6 +2,9 @@ DO $$
 BEGIN
     -- pj_empresas
     IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema='{schema}' AND table_name='pj_empresas') THEN
+        IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'pkey_pj_empresas' AND conrelid = '{schema}.pj_empresas'::regclass) THEN
+            ALTER TABLE IF EXISTS {schema}.pj_empresas ADD CONSTRAINT pkey_pj_empresas PRIMARY KEY (id);
+        END IF;
         IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'unique_pj_empresas_cnpj' AND conrelid = '{schema}.pj_empresas'::regclass) THEN
             ALTER TABLE {schema}.pj_empresas ADD CONSTRAINT unique_pj_empresas_cnpj UNIQUE (cnpj);
         END IF;
@@ -14,24 +17,31 @@ BEGIN
         END IF;
     END IF;
 
+    -- pj_empresas_enderecos
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema='{schema}' AND table_name='pj_empresas_enderecos') THEN
+        IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'unique_pj_empresas_enderecos_cnpj' AND conrelid = '{schema}.pj_empresas_enderecos'::regclass) THEN
+            ALTER TABLE {schema}.pj_empresas_enderecos ADD CONSTRAINT unique_pj_empresas_enderecos_cnpj UNIQUE (cnpj);
+        END IF;
+    END IF;
+
     -- pj_empresas_cnaes
     IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema='{schema}' AND table_name='pj_empresas_cnaes') THEN
-        IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'pj_empresas_cnaes_pkey' AND conrelid = '{schema}.pj_empresas_cnaes'::regclass) THEN
-            ALTER TABLE IF EXISTS {schema}.pj_empresas_cnaes ADD CONSTRAINT pj_empresas_cnaes_pkey PRIMARY KEY (id);
+        IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'pkey_pj_empresas_cnaes' AND conrelid = '{schema}.pj_empresas_cnaes'::regclass) THEN
+            ALTER TABLE IF EXISTS {schema}.pj_empresas_cnaes ADD CONSTRAINT pkey_pj_empresas_cnaes PRIMARY KEY (id);
         END IF;
     END IF;
 
     -- pj_cnaes_list
     IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema='{schema}' AND table_name='pj_cnaes_list') THEN
-        IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'pj_cnaes_list_pkey' AND conrelid = '{schema}.pj_cnaes_list'::regclass) THEN
-            ALTER TABLE IF EXISTS {schema}.pj_cnaes_list ADD CONSTRAINT pj_cnaes_list_pkey PRIMARY KEY (id);
+        IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'pkey_pj_cnaes_list' AND conrelid = '{schema}.pj_cnaes_list'::regclass) THEN
+            ALTER TABLE IF EXISTS {schema}.pj_cnaes_list ADD CONSTRAINT pkey_pj_cnaes_list PRIMARY KEY (id);
         END IF;
     END IF;
 
     -- pf_pessoas
     IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema='{schema}' AND table_name='pf_pessoas') THEN
-        IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'pf_pessoas_pkey' AND conrelid = '{schema}.pf_pessoas'::regclass) THEN
-            ALTER TABLE IF EXISTS {schema}.pf_pessoas ADD CONSTRAINT pf_pessoas_pkey PRIMARY KEY (id);
+        IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'pkey_pf_pessoas' AND conrelid = '{schema}.pf_pessoas'::regclass) THEN
+            ALTER TABLE IF EXISTS {schema}.pf_pessoas ADD CONSTRAINT pkey_pf_pessoas PRIMARY KEY (id);
         END IF;
     END IF;
 END $$;
