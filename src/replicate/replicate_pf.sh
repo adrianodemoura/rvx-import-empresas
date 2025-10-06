604c901c5570d55be42cea017f8239f775dac5c2
Adriano Moura <adrianodemoura@gmail.com>
2025-10-05 22:43:43 -0300

    correção ao replicar

--- src/replicate/replicate_pf.sh
+++ src/replicate/replicate_pf.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 source "./config/config.sh"
-LOG_NAME='replicate'
 
+EXECUTION_MODE="${1:-update}"
+LOG_NAME='replicate'
 LAST_ID_TO_IMPORT=0
-LAST_UPDATED_AT=0
+LAST_IMPORTED_AT=0
 LAST_SAVED_ID=0
-readonly SALT_ID_TO_IMPORT=10000
+readonly SALT_ID_TO_IMPORT=1000
 readonly NUM_INSTANCES=10
 readonly table_main='pf_pessoas'
 readonly tables=(
+    pf_escolaridade.escolaridade
+    pf_score.score
     pf_banco_gov.banco_gov
     pf_bolsa_familia.bolsa_familia
     pf_capacidade_pagamento.capacidade_pagamento
@@ -17,7 +20,6 @@ readonly tables=(
     pf_classe_social.classe_social
     pf_emails.emails
     pf_enderecos.enderecos
-    pf_escolaridade.escolaridade
     pf_fgts.fgts
     pf_governos.governos
     pf_imoveis_ibge.imoveis_ibge
@@ -25,12 +27,11 @@ readonly tables=(
     pf_nacionalidade.nacionalidade
     pf_obitos.obitos
     pf_persona_demografica.persona_demografica
+    pf_propensao_pagamento.propensao_pagamento
+    pf_renda.renda
     pf_pis.pis
     pf_poder_aquisitivo.poder_aquisitivo
     pf_politicamente_exposta.politicamente_exposta
-    pf_propensao_pagamento.propensao_pagamento
-    pf_renda.renda
-    pf_score.score
     pf_score_digital.score_digital
     pf_situacao_receita.situacao_receita
     pf_telefones.telefones
@@ -60,17 +61,15 @@ checkStart() {
     LAST_ID_TO_IMPORT=$("${PSQL_CMD[@]}" -t -A -F "" -c "SELECT id FROM $POSTGRES_DB_SCHEMA_FINAL.$table_main ORDER BY id DESC LIMIT 1")
     writeLog "🏁 Último ID de '$table_main': $(format_number $LAST_ID_TO_IMPORT)"
 
-    # recuperando o último ID SALVO
-    LAST_SAVED_ID=$(( (x=$(cat "$DIR_CACHE/replicate_last_saved_id" 2>/dev/null || echo "0")) > 0 ? x : LAST_SAVED_ID ))
-    # LAST_SAVED_ID=$("${MONGO_CMD[@]}" --quiet --eval 'db.pf_pessoas.find().sort({id: -1}).limit(1).toArray()[0].id')
-    # LAST_SAVED_ID=$(echo "954" | tr -d '.')
-    writeLog "🏁 Último ID já salvo: $(format_number $LAST_SAVED_ID)"
-    if [[ $LAST_SAVED_ID -eq 0 ]]; then # Se não tem ULTIMO ID para salvar, então vai pela atualização.
-        local OUT=$("${MONGO_CMD[@]}" --quiet --eval "db.getCollection('$table_main').findOne({}, { updated_at: 1, _id: 0 })?.updated_at")
-        LAST_UPDATED_AT=${OUT:-0}
-        writeLog "🔄 Iniciando a replicação com dados MAIOR QUE '$LAST_UPDATED_AT'"
+    # recuperando o último ID SALVO, caso NÃO seja modo de atualização
+    if [[ "$EXECUTION_MODE" != "update" ]]; then
+        LAST_SAVED_ID=$(( (x=$(cat "$DIR_CACHE/replicate_last_saved_id" 2>/dev/null || echo "0")) > 0 ? x : LAST_SAVED_ID ))
+        writeLog "🏁 Último ID já salvo: $(format_number $LAST_SAVED_ID)"
+    else
+        LAST_IMPORTED_AT=$("${MONGO_CMD[@]}" --quiet --eval "db.getCollection('$table_main').findOne({}, { imported_at: 1, _id: 0 })?.imported_at" | sed 's/-[0-9]\{2\}:[0-9]\{2\}$//')
+        LAST_IMPORTED_AT=${LAST_IMPORTED_AT:-0}
+        writeLog "🔄 Iniciando a replicação com dados MAIOR QUE '$LAST_IMPORTED_AT'"
     fi
-
     echo ""
 }
 
@@ -82,44 +81,67 @@ checkEnd() {
 }
 
 replicateWithSubcollections() {
+    local start_id=$1
+    local end_id=$2
     local SQL OUT START_TIME_REPLICATE=$(date +%s%3N)
-    local start_id=$1 end_id=$2 table="" nick="" offset=0
-    local dif_ids=$(( $end_id - $start_id +1 ))
+    local dif_ids=$(( end_id - start_id + 1 ))
 
-    [[ -z "$1" || -z "$2" ]] && { writeLog "❌ Erro: Os IDs inicial e final são obrigatórios!"; exit 1; }
+    [[ -z "$start_id" || -z "$end_id" ]] && {
+        writeLog "❌ Erro: IDs inicial e final obrigatórios!"; 
+        exit 1;
+    }
 
-    # Montando a SQL que vai buscar pf_pessoas no postgres
-    SQL="SELECT p1.cpf AS _id, p1.*, now() AS imported_at" 
+    # ============================================================
+    # 🔧 Monta lista de colunas
+    # ============================================================
+    local SQL_COLS="p1.cpf AS _id, p1.*, now() AS imported_at"
+
     for item in "${tables[@]}"; do
-        table=${item%%.*}
-        nick=${item#*.}
-        SQL+=", COALESCE( (SELECT json_agg($nick.*) FROM $POSTGRES_DB_SCHEMA_FINAL.$table $nick WHERE $nick.cpf = p1.cpf), '[]') AS $nick"
-    done
-    SQL+=" FROM $POSTGRES_DB_SCHEMA_FINAL.$table_main p1"
-    SQL+=" WHERE p1.id >= $start_id AND p1.id <= $end_id"
-    [[ "$LAST_UPDATED_AT" > 0 && $LAST_SAVED_ID == 0 ]] && SQL+=" AND p1.updated_at > '$LAST_UPDATED_AT'"
-    SQL+=" ORDER BY p1.id"
+        local table=${item%%.*} nick=${item#*.}
 
-    # Configurando para retornar JSON
-    SQL="SELECT row_to_json(t) FROM ( $SQL ) t;"
+        SQL_COLS+=", COALESCE(( SELECT json_agg($nick.*) FROM $POSTGRES_DB_SCHEMA_FINAL.$table $nick WHERE $nick.cpf = p1.cpf"
+        # [ "$EXECUTION_MODE" == "update" ] && {
+        #     SQL_COLS+=" AND $nick.updated_at > '$LAST_IMPORTED_AT'";
+        # }
+        SQL_COLS+="), '[]') AS $nick"
+    done
+
+    # ============================================================
+    # 🔧 Monta SQL final
+    # ============================================================
+    SQL="SELECT row_to_json(t) FROM (SELECT $SQL_COLS FROM $POSTGRES_DB_SCHEMA_FINAL.$table_main p1 WHERE p1.id BETWEEN $start_id AND $end_id"
+    [ "$EXECUTION_MODE" == "update" ] && {
+        SQL+=" AND p1.updated_at > '$LAST_IMPORTED_AT'";
+    }
+    SQL+=" ORDER BY p1.id) t;"
+
+    # Salva SQL gerada para debug
     echo "$SQL" > "$DIR_CACHE/replicate_last_sql"
 
-    writeLog "🔎 Aguarde a BUSCA da faixa $(format_number $start_id)/$(format_number $end_id) com $(format_number $dif_ids) linhas no postgreSQL remoto..."
+    # ============================================================
+    # 🚀 Execução
+    # ============================================================
+    writeLog "🔎 Buscando faixa $(format_number $start_id)/$(format_number $end_id) com $(format_number $dif_ids) linhas..."
     OUT=$("${PSQL_CMD[@]}" -t -A -F "" -c "$SQL")
+    echo "$OUT" > "$DIR_CACHE/replicate_last_out"
+
     if [[ -z "$OUT" ]]; then
-        writeLog "📦 Nenhum dado retornado na faixa $start_id/$end_id"
+        writeLog "📦 Nenhum dado retornado na faixa $(format_number $start_id)/$(format_number $end_id)."
     else
-        writeLog "🔎 Aguarde a INSERÇÃO/ATUALIZAÇÃO da faixa $(format_number $start_id)/$(format_number $end_id) com $(format_number $dif_ids) linhas no mongoDB"
-        echo "$OUT" | "${MONGOIMPORT_CMD[@]}" --collection "$table_main" --mode upsert --upsertFields _id --type json > /dev/null 2>&1
-        if [[ $? -ne 0 ]]; then
-            writeLog "❌ Erro ao importar lote OFFSET $offset. Abortando..."
-            exit 1
-        fi
+        writeLog "📦 $([ "$EXECUTION_MODE" == "update" ] && echo "Atualizando" || echo "Inserindo") a faixa $(format_number $start_id)/$(format_number $end_id)..."
+        echo "$OUT" | "${MONGOIMPORT_CMD[@]}" \
+            --collection "$table_main" \
+            --mode upsert \
+            --upsertFields _id \
+            --type json > /dev/null 2>&1
 
-        # salva o maior ID replicado no mongoDB
-        [ $end_id -gt $(cat "$DIR_CACHE/replicate_last_saved_id" 2>/dev/null || echo 0) ] && echo "$end_id" > "$DIR_CACHE/replicate_last_saved_id"
+        [ $? -ne 0 ] && { writeLog "❌ Erro ao importar lote $start_id-$end_id. Abortando..."; exit 1; }
 
-        writeLog "📦 Faixa $(format_number $start_id)/$(format_number $end_id) com $(format_number $dif_ids) linhas replicadas com sucesso em $(calculateExecutionTime $START_TIME_REPLICATE)"
+        # Atualiza o maior ID salvo
+        [ $end_id -gt $(cat "$DIR_CACHE/replicate_last_saved_id" 2>/dev/null || echo 0) ] \
+            && echo "$end_id" > "$DIR_CACHE/replicate_last_saved_id"
+
+        writeLog "✅ Faixa $(format_number $start_id)/$(format_number $end_id) replicada com sucesso em $(calculateExecutionTime $START_TIME_REPLICATE)"
     fi
 }
 
@@ -127,8 +149,7 @@ replicateWithSubcollections() {
 
 checkStart
 
-writeLog "🏁 Iniciando a replicação de $(format_number $TOTAL_RECORDS_TO_IMPORT) registros do postgres..."
-
+writeLog "🏁 Iniciando a replicação do postgreSQL para mongoDB no modo '$EXECUTION_MODE'..."
 for ((current_id=LAST_SAVED_ID; current_id<=LAST_ID_TO_IMPORT; current_id+=SALT_ID_TO_IMPORT)); do
     BATCH_SIZE=$(( SALT_ID_TO_IMPORT / NUM_INSTANCES ))
 
@@ -141,13 +162,14 @@ for ((current_id=LAST_SAVED_ID; current_id<=LAST_ID_TO_IMPORT; current_id+=SALT_
         # Verificações para a faixa
         [ $end_id -gt $LAST_ID_TO_IMPORT ] && { end_id=$LAST_ID_TO_IMPORT; }
         [ $start_id -gt $LAST_ID_TO_IMPORT ] && { continue; }
-        [ $end_id -gt $(cat "$DIR_CACHE/replicate_last_saved_id" 2>/dev/null || echo 0) ] && echo "$end_id" > "$DIR_CACHE/replicate_last_saved_id"
+        [ $end_id -gt $(cat "$DIR_CACHE/replicate_last_saved_id" 2>/dev/null || echo 0) ] \
+            && echo "$end_id" > "$DIR_CACHE/replicate_last_saved_id"
 
         # writeLog "$current_id) sub-loop: $current_instance start_id: $start_id end_id: $end_id"
         replicateWithSubcollections $start_id $end_id &
     done
     wait
-    echo
+    echo ""
 done
 
 checkEnd
